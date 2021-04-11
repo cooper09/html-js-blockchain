dd8c3c862580f71015ec1b7bbbdae934d3695dc6
cooper09 <cooper@sonyainc.net>
2021-04-11 12:35:55 -0400

    added transactions and mining awards

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
+class Transaction {
+    constructor (fromAddress, toAddress, amount ){
+        this.fromAddress = fromAddress;
+        this.toAddress = toAddress;
+        this.amount = amount;
 
+    }//end constructor
+
+}//end Transaction
 
 class Block {
-    constructor (index,timestamp, data, previousHash ='') {
-        this.index = index;
+    constructor (timestamp, transactions, previousHash ='') {
+        //this.index = index;
         this.timestamp = timestamp;
-        this.data = data;
+        this.transactions = transactions;
         this.previousHash = previousHash;
         this.hash = this.createHash();
         //add nonce for mining
@@ -34,11 +42,13 @@ class Blockchain {
     constructor () {
         this.chain = [this.createGenesisBlock()];
         this.difficulty = 2;
+        this.pendingTransactions = [];
+        this.miningReward = 100;
     }
     //create genesis block
     createGenesisBlock () {
         const time = new Date().getTime();
-        return new Block(0, time ,'Genesis Block' , '0' );
+        return new Block( time ,'Genesis Block' , '0' );
     }
     //get last block
     getLatestBlock() {
@@ -46,13 +56,48 @@ class Blockchain {
         return this.chain[this.chain.length - 1];
     }
     // create new block
-    addBlock(newBlock) {
+   /*addBlock(newBlock) {
         //console.log("newBlock: ", this.getLatestBlock().hash);
         newBlock.previousHash = this.getLatestBlock().hash;
         //newBlock.hash = newBlock.createHash();
         newBlock.mineBlock(this.difficulty);
         this.chain.push(newBlock);
-    }
+    } */
+
+    //Instead of mining block by block, queue them up as transactions and go from there...
+    minePendingTransactions (miningRewardAddress) {
+        let block = new Block(Date.now(), this.pendingTransactions);
+        block.mineBlock(this.difficulty);
+
+        console.log("Transaction block successfully mined.");
+        this.chain.push(block);
+
+        this.pendingTransactions = [
+            new Transaction(null, miningRewardAddress,this.miningReward )
+        ]
+    }//end minePendingTransactions
+
+    createTransaction(transaction) {
+        this.pendingTransactions.push(transaction);
+    }//end createTransaction
+
+    getBalanceOfAddress (address) {
+        console.log("Address balance: ", address )
+        let balance = 0;
+        for (const block of this.chain ) {
+            for (const trans of block.transactions) {
+                if (trans.fromAddress === address) {
+                    balance -= trans.amount;
+                }//end if
+                
+                if (trans.toAddress === address) {
+                    balance += trans.amount;
+                }//end if
+            }//end for
+        }//end for
+        return balance;
+    }//end getBalanceOf
+
     // validate block
     validate() {
         for (let i=1 ; i < this.chain.length; i++) {
