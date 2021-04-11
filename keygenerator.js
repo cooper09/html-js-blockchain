648d24bfa39e02ac0e6099a3c4737df5c9302450
cooper09 <cooper@sonyainc.net>
2021-04-11 16:09:20 -0400

    lets display our blockchain

---
+++ keygenerator.js
@@ -0,0 +1,11 @@
+// install some kind of key generator
+
+function keyPair() {
+    console.log("generate keyPair")
+    const keyObj = {
+        privateKey: "0x0",
+        publicMey: "0x1"
+    }
+    return keyObj
+}
+//module.exports.keyPair = keyPair;
